d1f97f41a6d1b37056982339df7a6c1c3a8ba84e
Emilie Gouy <vibesession@gmail.com>
2021-12-08 16:21:40 +0100

    destroy ne fonctionne pas

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,69 +1,52 @@
 class RestaurantsController < ApplicationController
-  before_action :set_restaurant, only: %i[ show ]
+   before_action :set_restaurant, only: %i[ show edit update destroy ]
 
-  # GET /restaurants or /restaurants.json
   def index
     @restaurants = Restaurant.all
   end
 
-  # GET /restaurants/1 or /restaurants/1.json
   def show
   end
 
-  # GET /restaurants/new
   def new
     @restaurant = Restaurant.new
   end
 
-  # GET /restaurants/1/edit
   def edit
   end
 
-  # POST /restaurants or /restaurants.json
   def create
     @restaurant = Restaurant.new(restaurant_params)
-
-    respond_to do |format|
       if @restaurant.save
-        format.html { redirect_to @restaurant, notice: "Restaurant was successfully created." }
-        format.json { render :show, status: :created, location: @restaurant }
+       redirect_to @restaurant
       else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
+        render :new
       end
-    end
   end
 
   # PATCH/PUT /restaurants/1 or /restaurants/1.json
   def update
-    respond_to do |format|
-      if @restaurant.update(restaurant_params)
-        format.html { redirect_to @restaurant, notice: "Restaurant was successfully updated." }
-        format.json { render :show, status: :ok, location: @restaurant }
+    @restaurant.update(restaurant_params)
+      if @restaurant.save
+        redirect_to @restaurant
       else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @restaurant.errors, status: :unprocessable_entity }
+        render :edit
       end
-    end
   end
 
   # DELETE /restaurants/1 or /restaurants/1.json
   def destroy
     @restaurant.destroy
-    respond_to do |format|
-      format.html { redirect_to restaurants_url, notice: "Restaurant was successfully destroyed." }
-      format.json { head :no_content }
-    end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_restaurant
-      @restaurant = Restaurant.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def restaurant_params
-      params.require(:restaurant).permit(:name, :address, :category, :phone_number)
-    end
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:id])
+  end
+
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :phone_number, :category, :restaurant_id)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :restaurants
+  resources :restaurants do
+    resources :reviews, only: [:new, :create] # -> restaurants/restaurant_id/reviews/new
+  end
+
+  resources :reviews, only: [:destroy]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
